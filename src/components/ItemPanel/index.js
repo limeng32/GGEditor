0690c1848f47e3236f0640aa68171d7190cc65a1
李萌 <limengag@yonyou.com>
2023-11-14 20:41:04 +0800

    feat: 加入antd依赖；

--- src/components/ItemPanel/index.js
+++ src/components/ItemPanel/index.js
@@ -1,14 +1,30 @@
+/* eslint-disable arrow-spacing */
+/* eslint-disable space-infix-ops */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable key-spacing */
+/* eslint-disable lines-between-class-members */
+/* eslint-disable no-extra-semi */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable indent */
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/jsx-boolean-value */
+/* eslint-disable import/newline-after-import */
+/* eslint-disable import/order */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
 import React from 'react';
 import { pick } from '@utils';
 import withGGEditorContext from '@common/context/GGEditorContext/withGGEditorContext';
 import Item from './Item';
-
+import { Modal } from 'antd';
 class ItemPanel extends React.Component {
   page = null;
 
   constructor(props) {
     super(props);
-
+    this.state = {
+      isModalOpen: true,
+  };
     this.bindEvent();
   }
 
@@ -32,9 +48,19 @@ class ItemPanel extends React.Component {
 
   render() {
     const { children } = this.props;
-
+    const handleOk = () => {
+      this.setState({isModalOpen:false});
+    };
+    const handleCancel = () => {
+      this.setState({isModalOpen:false});
+    };
     return (
       <div id={this.containerId} {...pick(this.props, ['style', 'className'])}>
+        <Modal title="Basic Modal" open={this.state.isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
         {children}
       </div>
     );
